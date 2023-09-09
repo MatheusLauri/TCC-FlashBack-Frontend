4c10031c01a77e0ff7a636f823f3766d90ec3006
MatheusLauri <matheuslauri2509@gmail.com>
2023-09-09 13:31:52 -0300

    comecei rodapé ( n acabou )

---
+++ src/componentes/rodape/index.js
@@ -0,0 +1,29 @@
+import './index.scss';
+
+function Rodape() {
+  return (
+    <div className="main-rodape">
+        
+        <h1>Redes sociais</h1>
+
+        <div className='fotos-cab'>
+            <p>f1</p>
+            <p>f2</p>
+            <p>f3</p>
+            <p>f4</p>
+        </div>
+
+        <div className='barra-long'>
+            <p>txt 1</p>
+            <p>txt 2</p>
+            <p>txt 3</p>
+        </div>
+
+        <div className='textos-cab'>
+
+        </div>
+    </div>
+  );
+}
+
+export default Rodape; 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
+
+
 import Ladingpage from './ladingpage/index.js';
+import Rodape from './componentes/rodape/index.js'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,7 +13,10 @@ root.render(
   <React.StrictMode>
       <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Ladingpage/>} />
+            
+            <Route path='/' element={<Ladingpage/>}/>
+            <Route path='/rodape' element={<Rodape/>}/>
+
         </Routes>
       </BrowserRouter>
   </React.StrictMode>
