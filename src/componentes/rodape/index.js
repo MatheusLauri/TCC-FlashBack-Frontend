726c7fcf68438285b0e0a54e4fb2912e00a67bbe
MatheusLauri <matheuslauri2509@gmail.comm>
2023-09-11 15:24:15 -0300

    comecei rodape ( n acabou, falta imagem )

--- src/componentes/rodape/index.js
+++ src/componentes/rodape/index.js
@@ -14,13 +14,13 @@ function Rodape() {
         </div>
 
         <div className='barra-long'>
-            <p>txt 1</p>
-            <p>txt 2</p>
-            <p>txt 3</p>
+
         </div>
 
         <div className='textos-cab'>
-
+            <p>Todos os preços e condições comerciais estão sujeitos a alterração comercial sem prévio aviso</p>
+            <p className='t2'>FlashBack Tecnolia e Soluções LTDA - CNPJ 10.316.295/0001-05</p>
+            <p>Rua laurinopólis, 303,  C|224 - CEP - 03110-010 - São Paulo - SP</p>  
         </div>
     </div>
   );
