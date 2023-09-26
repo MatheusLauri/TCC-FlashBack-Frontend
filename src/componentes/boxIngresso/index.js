55e1503f504e44e11c4ade883dd6fc47b2d52b21
JoaoMaFase <nascc.joao@gmail.com>
2023-09-26 08:11:12 -0300

    ok

--- src/componentes/boxIngresso/index.js
+++ src/componentes/boxIngresso/index.js
@@ -3,7 +3,7 @@ import './index.scss'
 export default function BoxIngresso(props){
     return(
         <div className='box-ingresso-main'>
-            <div className='box-ingresso-bg' style={{backgroundImage: `url('${props.src}')`}}></div>
+            <img className='box-ingresso-bg' src={props.src/>
             <div className='box-ingresso-content'>
                 <div className='box-ingresso-adress'><h1>{props.data}</h1></div>
                 <div className='box-ingresso-name'>
--- src/landingpage/index.js
+++ src/landingpage/index.js
@@ -81,7 +81,6 @@ function LandingPage() {
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none"><path d="M16 32C7.16344 32 0 24.8366 0 16C0 7.16344 7.16344 0 16 0C24.8366 0 32 7.16344 32 16C32 24.8366 24.8366 32 16 32ZM16 30C23.732 30 30 23.732 30 16C30 8.26801 23.732 2 16 2C8.26801 2 2 8.26801 2 16C2 23.732 8.26801 30 16 30ZM13.9289 23.1L12.5147 21.6858L18.193 16.008L12.5147 10.3289L13.9289 8.91472L21 15.9858L20.979 16.008L21 16.0289L13.9289 23.1Z" fill="#520DA9"/></svg>
           </div>
           <div className='carrosel'>
-            
               {listar.map(item =>
                 <BoxIngresso nome={item.NM_EVENTO} data={item.DT_COMECO} imagem={item.IMAGEM_INGRESSO}/> 
               )}
