8d425034406333ccfd6cd51071e42a57cbfe103f
EricTavares16 <ets1603@gmail.com>
2023-09-26 00:00:11 -0300

    amostra cards ingresso

--- src/componentes/boxIngresso/index.js
+++ src/componentes/boxIngresso/index.js
@@ -7,7 +7,7 @@ export default function BoxIngresso(props){
             <div className='box-ingresso-content'>
                 <div className='box-ingresso-adress'><h1>{props.data}</h1></div>
                 <div className='box-ingresso-name'>
-                    <h1>{props.evento}</h1>
+                    <h1>{props.nome}</h1>
                     <p>{props.local}</p>
                 </div>
             </div>
