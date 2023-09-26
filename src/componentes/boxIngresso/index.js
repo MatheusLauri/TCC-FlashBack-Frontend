76545cfdfc4b5ee4377b6534006e093346367d60
JoaoMaFase <nascc.joao@gmail.com>
2023-09-26 08:11:35 -0300

    ok

--- src/componentes/boxIngresso/index.js
+++ src/componentes/boxIngresso/index.js
@@ -3,7 +3,7 @@ import './index.scss'
 export default function BoxIngresso(props){
     return(
         <div className='box-ingresso-main'>
-            <img className='box-ingresso-bg' src={props.src/>
+            <img className='box-ingresso-bg' src={props.src}/>
             <div className='box-ingresso-content'>
                 <div className='box-ingresso-adress'><h1>{props.data}</h1></div>
                 <div className='box-ingresso-name'>
