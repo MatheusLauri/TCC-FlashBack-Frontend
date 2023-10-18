08cc794e1e0fd9baee9fdc8a9904b82df2053bc3
MatheusLauri <matheuslauri2509@gmail.com>
2023-10-18 11:47:11 -0300

    ok

--- src/componentes/admTicket/index.js
+++ src/componentes/admTicket/index.js
@@ -22,11 +22,25 @@ export default function AdmTicket(props) {
     //IMAGEM
     const urlImagem = `http://localhost:5000/${props.imagem}`
 
+    const Tipos = []
+    const [click,setClick] = useState(false)
+    const [ListarTipos,setListarTipos] = useState([])
+    async function ListarTipoIngresso(){
+        let r = `http://localhost:5000/tipoIngresso/${props.id}`
+        let response = await axios.get(r)
+        Tipos.push(response)
+        setListarTipos(Tipos)
+        console.log(ListarTipos)
+    }
+    useEffect(() => {
+        ListarTipoIngresso()
+    }, [click])
+
     return (
         <div className={showType ? 'adm-ticket-grow' : 'adm-ticket'}>
             <div className='ticket-upper-info'>
-                <p onClick={(e) => console.log(props)}>ID: <span>{props.id}</span></p>
-                <h1>{props.nome}</h1>
+                <p>ID: <span>{props.id}</span></p>
+                <h1 onClick={() => ListarTipoIngresso()}>{props.nome}</h1>
                 <div className='ticket-controller'>
                     <img src='../assets/images/edit.svg'/>
                     <img src='../assets/images/delete.svg'/>
@@ -38,34 +52,18 @@ export default function AdmTicket(props) {
                     <p className='last-child'>Local: <span>Teatro Municipal - São Paulo, SP</span></p>
                     <div className={showType ? 'ticket-type-clicked' : 'ticket-type'}>
                         <div className='header'>
-                            <a onClick={() => setShowType(!showType)}>Tipos Ingresso, qtd e valor</a>
+                            <a onClick={() => {setShowType(!showType); setClick(!click)}}>Tipos Ingresso, qtd e valor</a>
                             <img src='../assets/images/arrow.svg' onClick={() => setShowType(!showType)}/>
                         </div>
                         <div className='body'>
-                            <div className='body-row'>
-                                <span>Front Stage:</span>
-                                <span>15 Un</span>
-                                <div></div>
-                                <span>R$ 150,00</span>
-                            </div>
-                            <div className='body-row'>
-                                <span>Front Stage:</span>
-                                <span>15 Un</span>
-                                <div></div>
-                                <span>R$ 150,00</span>
-                            </div>
-                            <div className='body-row'>
-                                <span>Front Stage:</span>
-                                <span>15 Un</span>
-                                <div></div>
-                                <span>R$ 150,00</span>
-                            </div>
-                            <div className='body-row'>
-                                <span>Front Stage:</span>
-                                <span>15 Un</span>
-                                <div></div>
-                                <span>R$ 150,00</span>
-                            </div>
+                            {ListarTipos.map(item => {
+                                <div className='body-row'>
+                                    <span>{props.nomeTipo}</span>
+                                    <span>{props.qtd} Un</span>
+                                    <div></div>
+                                    <span>R$ {props.valor}</span>
+                                </div>
+                            })}
                         </div>
                     </div>
                 </div>
