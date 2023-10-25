2fe3ed9248275cadbe701f442a17eee30ef2be2d
MatheusLauri <matheuslauri2509@gmail.com>
2023-10-25 13:50:01 -0300

    card-meupedido feito

--- src/componentes/card-Meuspedidos/index.js
+++ src/componentes/card-Meuspedidos/index.js
@@ -4,41 +4,40 @@ import './index.scss'
 export default function Card() {
 
 return (
-    <section>
+    <div className='primaria'>
         <div className='mae-geral'>
             
             <div className='left-side'>
                 <img src='./assets/images/farofada.png' />
 
-                <div className='informacoes-rigth'>
+                <div className='informacoes-left'>
 
-                    <p>DATA</p>
+                    <p>Sáb, 30 Nov - 20:00</p>
 
                     <div className='infos-filhos'>
                         <h3>Alok & Jeffersson M.</h3>
-                        <h5>Local</h5>
+                        <p>Teatro Municipal - São Paulo, SP </p>
                     </div>
                 </div>
             </div>
 
             <div className='rigth-side'>
-                <div className='informacoes-left'>
+                <div className='informacoes-rigth'>
                         <div className='filhos-left'>
-                                <h3>Disponivel</h3>
+                                <h3>Disponivel:</h3>
                                 <p> sim </p>
                         </div>
 
                         <div className='filhos-rigth'>
-                                <h3>Status</h3>
+                                <h3>Status:</h3>
                                 <p> FInalizado </p>
-
                         </div>
                 </div>
 
-                <button> osh </button>
+                <button> Visualizar </button>
             </div>
         </div>
-    </section>
+    </div>
 );
 
 }
