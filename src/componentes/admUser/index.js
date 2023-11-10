4b621fce0d66ed1d3c0cc82c7070f359548b986d
JoaoMaFase <nascc.joao@gmail.com>
2023-11-10 11:29:28 -0300

    painel adm

--- src/componentes/admUser/index.js
+++ src/componentes/admUser/index.js
@@ -6,7 +6,7 @@ export default function AdmUser(props) {
         <div className='user-bar'>
             <h1>{props.page}</h1>
             <div>
-                <span>Olá novamente, {props.user}</span>
+                <span>Olá novamente, <span>{props.user}</span></span>
                 <span>{props.funcao}</span>
             </div>
         </div>
