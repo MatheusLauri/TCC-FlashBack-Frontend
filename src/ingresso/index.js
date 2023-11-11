6a951ddd3ed7dd3527bdb543c37996264eec3ed7
EricTavares16 <ets1603@gmail.com>
2023-11-11 17:08:46 -0300

    datas do ingresso comprar pronta

--- src/ingresso/index.js
+++ src/ingresso/index.js
@@ -11,9 +11,19 @@ import local from '../assets/images/local.png'
 
 export default function IngressoPage(){
     let {id} = useParams()
+
     const [showDescription,setShowDescription] = useState(false)
     const [ingressos,setIngressos] = useState([]) 
     const [tipoIngressos,setTipoIngressos] = useState([]) 
+
+
+    //variaveis listagem data
+
+    const [listarDatas, setListarDatas] = useState([]) 
+    // const [idDatas, setIdDatas] = useState([]) 
+    const [idDatas, setIdDatas] = useState(1) 
+    const [listarHorarios, setListarHorarios] = useState([]) 
+
     
     async function ListarIngressos(){
         let url = `http://localhost:5000/ingresso/busca?nome`
@@ -28,6 +38,7 @@ export default function IngressoPage(){
     
     useEffect(() => {
         ListarIngressos()
+        ListarData_Comprar()
     },[id])
     let url = `http://localhost:5000/${ingressos.IMAGEM_INGRESSO}`
     
@@ -35,6 +46,35 @@ export default function IngressoPage(){
     const datetime = new Date(ingressos.DT_COMECO);
     const monthNames = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
     const formattedDate = `${datetime.getDate()} ${monthNames[datetime.getMonth()]} - ${datetime.getFullYear()} | ${datetime.getHours()}:${datetime.getMinutes()}`;
+
+
+    //Listar Datas e horarios do Ingresso
+
+    async function ListarData_Comprar () {
+        const resp = await axios.get(`http://localhost:5000/data/compra/${id}`)
+
+        
+        setListarDatas(resp.data)
+
+    //     const guardarDatas = []
+        
+    //     for(let item of resp.data) {
+    //         guardarDatas.push(item.ID_DATA_INGRESSO)
+    //     }
+ 
+    //    setIdDatas(guardarDatas)
+    }
+
+    
+    async function ListarHorario (idData) {
+
+        const resp = await axios.get(`http://localhost:5000/horario/compra/${idData}`)
+    
+        setListarHorarios(resp.data)
+
+    }
+
+
 
     return (
         <div className='ingresso-body'>
@@ -70,48 +110,33 @@ export default function IngressoPage(){
                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none"><path d="M16 32C7.16344 32 0 24.8366 0 16C0 7.16344 7.16344 0 16 0C24.8366 0 32 7.16344 32 16C32 24.8366 24.8366 32 16 32ZM16 30C23.732 30 30 23.732 30 16C30 8.26801 23.732 2 16 2C8.26801 2 2 8.26801 2 16C2 23.732 8.26801 30 16 30ZM13.9289 23.1L12.5147 21.6858L18.193 16.008L12.5147 10.3289L13.9289 8.91472L21 15.9858L20.979 16.008L21 16.0289L13.9289 23.1Z" fill="#520DA9"/></svg>
                         </div>
                         <div className='data-controller'>
-                            <div className='data-box'>
-                                <h1>Segunda-feira</h1>
-                                <p>12 set</p>
-                            </div>
-                            <div className='data-box'>
-                                <h1>Terça-feira</h1>
-                                <p>12 set</p>
-                            </div>
-                            <div className='data-box'>
-                                <h1>Quarta-feira</h1>
-                                <p>12 set</p>
-                            </div>
+
+                            {listarDatas.map((item) => (
+
+                                <div onClick={() => {setIdDatas(item.ID_DATA_INGRESSO); ListarHorario(idDatas); console.log(idDatas)}}  className='data-box'>
+                                    <h1>{item.DT_INGRESSO}</h1>
+                                    <p>12 set</p>
+                                </div>
+                            ))}
+                            
                         </div>
                         <div className='time-select'>
                             <h1>Selecione um horário</h1>
                             <div className='ticket-wrapper'>
-                                <div className='time-select-box'>
-                                    <h1>Horário 23:00</h1>
-                                    <p>Preços entre R$ 10,00 e R$ 100,00</p>
-                                    <p>em até 12x</p>
-                                </div>
-                                <div className='time-select-box'>
-                                    <h1>Horário 23:00</h1>
-                                    <p>Preços entre R$ 10,00 e R$ 100,00</p>
-                                    <p>em até 12x</p>
-                                </div>
+
+                                {idDatas &&
+                                    <>
+                                        {listarHorarios.map((item) => (
+                                            <div className='time-select-box'>
+                                                <h1>Horário {item.DS_HORARIO}</h1>
+                                                <p>Preços entre R$ 10,00 e R$ 100,00</p>
+                                                <p>em até 12x</p>
+                                            </div>
+                                        ))}
+                                    </>
+                                }
                                 
-                                <div className='time-select-box'>
-                                    <h1>Horário 23:00</h1>
-                                    <p>Preços entre R$ 10,00 e R$ 100,00</p>
-                                    <p>em até 12x</p>
-                                </div>
-                                <div className='time-select-box'>
-                                    <h1>Horário 23:00</h1>
-                                    <p>Preços entre R$ 10,00 e R$ 100,00</p>
-                                    <p>em até 12x</p>
-                                </div>
-                                <div className='time-select-box'>
-                                    <h1>Horário 23:00</h1>
-                                    <p>Preços entre R$ 10,00 e R$ 100,00</p>
-                                    <p>em até 12x</p>
-                                </div>
+                               
                             </div>
                         </div>
                     </div>
