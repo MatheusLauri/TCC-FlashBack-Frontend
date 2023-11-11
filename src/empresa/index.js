66661250087d0afcad58a9a68644fe3854f359b8
JoaoMaFase <nascc.joao@gmail.com>
2023-11-11 17:47:31 -0300

    ok

--- src/empresa/index.js
+++ src/empresa/index.js
@@ -1,3 +1,5 @@
+import { Header } from '../componentes/header/header'
+import Rodape from '../componentes/rodape'
 import './index.scss'
 
 
@@ -5,7 +7,14 @@ export default function Empresa(){
 
     return (
         <div className='empresa-main'>
-            
+            <section className='header'>
+                <Link className='header-img' to='/'><img src='/assets/images/logoTCC.png' /></Link>
+            </section>
+            <section className='secao-01'>
+                <h1>Produza eventos e conteúdos na maior plataforma do país.</h1>
+                <p>Produza eventos e conteúdos na maior plataforma do país.</p>
+            </section>
+            <Rodape/>
         </div>
     )
 }
